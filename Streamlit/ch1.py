ace49b370842d29235c1c0e7d57cdec64e0fc67e
Himanshi <152062813+hima879@users.noreply.github.com>
2026-07-26 21:47:12 +0530

    Add files via upload

--- Streamlit/ch1.py
+++ Streamlit/ch1.py
@@ -1,1 +1,15 @@
+try:
+    import streamlit as st
+    from typing import TYPE_CHECKING
+except ImportError:
+    raise ImportError("streamlit is not installed. Please run: pip install streamlit")
 
+st.title("Chai Streamlit Example")
+st.subheader("Brewed with Streamlit.")
+st.text("Welcome to your first interactive app")
+st.write("Choose your favorite chai:")
+
+chai = st.selectbox("Select a chai flavor", ["Masala Chai", "Ginger Chai", "Cardamom Chai", "Tulsi Chai"])
+st.write(f"You selected {chai}, Excellent choice! Enjoy your {chai}!")
+
+st.success("Your choice has been brewed to perfection! ☕️")
